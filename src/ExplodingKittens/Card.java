7de0d2222ca27ccea0ae41ff6157747a95ca842c
Joost080 <152535248+Joost080@users.noreply.github.com>
2023-12-19 14:02:41 +0100

    Merge branch 'check' into 'main'
    Check
    
    See merge request s3259633/exploding-kittens-project!4

--- src/ExplodingKittens/Card.java
+++ src/ExplodingKittens/Card.java
@@ -5,15 +5,6 @@ public class Card {
     enum Cards{
       EXPLODINGKITTEN, DEFUSE, NOPE, SHUFFLE, SKIP, SEETHEFUTURE, CATCARD, FAVOR, ATTACK
         //if its correct this is how it is correct
-//       ExplodingKitten,
-//        Defuse,
-//        Nope,
-//        Shuffle,
-//        Skip,
-//        SeeTheFuture,
-//        CatCard,
-//        Favor,
-//        Attack
     }
     // id; int
     public int id;
@@ -30,6 +21,7 @@ public class Card {
 
     }
     public void  Defuse(){
+        //card.remove
     }
     public void  Nope  (){
 
@@ -40,8 +32,7 @@ public class Card {
     public void  Skip(){
 
         }
-    public void  SeeTheFuture(){
-
+    public void  SeeTheFuture(){ 
         }
     public void  CatCard(){
 
--- src/ExplodingKittens/Deck.java
+++ src/ExplodingKittens/Deck.java
@@ -6,6 +6,13 @@ public class Deck {
     public Card Card;
     // cards: Arraylist<card>
     public ArrayList<Card> deck;
+    public ArrayList<Card> begindeck;
+    public void setBegindeck() {
+        begindeck.add(ExplodingKittens.Card.Cards.)
+
+        this.begindeck = begindeck;
+    }
+
     // shuffle() shuffles the cards
     public void shuffle(){
         //randomizes the cards order
@@ -13,6 +20,9 @@ public class Deck {
     //drawCard() takes a card from the top of the deck and removes from the deck
     public Card drawCard(){
         return Card;
+
+    }
+    public void removeCard(){
 
     }
 
