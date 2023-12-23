7aad9e5f347c5442f93586b33c2abc45a1d1114a
Joost080 <152535248+Joost080@users.noreply.github.com>
2023-12-23 15:36:45 +0100

    commited

--- src/ExplodingKittens/Deck.java
+++ src/ExplodingKittens/Deck.java
@@ -34,7 +34,7 @@ public class Deck {
             case DEFUSE:
                 return 6; //6
             case SKIP:
-                return 4;
+                return 10;
             case ATTACK:
                 return 4;
             case FAVOR:
@@ -42,7 +42,7 @@ public class Deck {
             case SEE_THE_FUTURE:
                 return 5;
             case SHUFFLE:
-                return 20; //4
+                return 4; //4
             case NOPE:
                 return 5;
             case CAT_CARD1, CAT_CARD2, CAT_CARD3, CAT_CARD4, CAT_CARD5:
